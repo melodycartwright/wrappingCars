6f24d64d80342c4fa653b5f79e8ccfe9dfdc8a6c
Melody Cartwright <melody94c@gmail.com>
2024-11-21 14:33:14 +0100

    Added html, css, script, and images. created structure for page

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+  function toggleMenu() {
+    const menu = document.querySelector('.menu-list');
+    menu.classList.toggle('active');
+  }
+
+// You can add custom JavaScript here if needed
+document.addEventListener('DOMContentLoaded', function () {
+  console.log("Custom JavaScript is working!");
+});
